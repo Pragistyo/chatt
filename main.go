4eef4bd3aef20330b9ae80f2a84556cf7bf486bd
Pragistyo <new.SOS.hope@outlook.com>
2020-11-26 20:26:10 +0700

    edit router get-message-chat-room & update-read : using param

--- main.go
+++ main.go
@@ -27,11 +27,11 @@ func main(){
 	api.HandleFunc("/message/",controllers.PostMessage).Methods(http.MethodPost)
 
 	//GET
-	api.HandleFunc("/message-chat-room/",controllers.GetMessagesChatRoom).Methods(http.MethodGet)
+	api.HandleFunc("/message-chat-room/{chat_room_name}/",controllers.GetMessagesChatRoom).Methods(http.MethodGet)
 	api.HandleFunc("/conversation-card/",controllers.CardConversation).Methods(http.MethodGet)
 
 	//PATCH
-	api.HandleFunc("/update-read-message/{chat_room_id}/{user_id}",controllers.UpdateMessagesRead).Methods(http.MethodPatch)
+	api.HandleFunc("/update-read-message/{chat_room_name}/{user_id}/",controllers.UpdateMessagesRead).Methods(http.MethodPatch)
 
 	PORT := os.Getenv( "PORT" )
 	log.Fatal(http.ListenAndServe(PORT, r))
