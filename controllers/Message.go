ad8738a48284539f2c47b2d47a31c5d15af8a405
Pragistyo <new.SOS.hope@outlook.com>
2020-11-26 08:09:01 +0700

    add controllers skeleton: CardConv, Login, ChatRoom, Message

---
+++ controllers/Message.go
@@ -0,0 +1,20 @@
+package controllers
+
+
+import (
+	"log"
+	"net/http"
+	db "github.com/Pragistyo/chatt/db"
+)
+
+func PostMessage(w http.ResponseWriter,r *http.Request){
+	
+}
+
+func GetMessagesChatRoom(w http.ResponseWriter,r *http.Request){
+
+}
+
+func UpdateMessagesRead(w http.ResponseWriter,r *http.Request){
+
+}
