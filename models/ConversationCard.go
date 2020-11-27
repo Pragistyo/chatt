b22bf6d2248e05c94e69e07ce7057a0fd89fe502
Pragistyo <new.SOS.hope@outlook.com>
2020-11-27 17:34:18 +0700

    add Id to model

--- models/ConversationCard.go
+++ models/ConversationCard.go
@@ -1,9 +1,14 @@
 package models
 
+import "database/sql"
+
+import(
+	"database/sql"
+)
 
 type ConversationCard struct {
-	Id  		    string   		  `json:"id"`
-	Name  		    string   		  `json:"name"`
-	UnreadCount 	int32    		  `json:"unread_count"`
-	LastMsg			string    	 	  `json:"last_msg"`
+	Id          string        		`json:"id"`
+	Name        string       	 	`json:"name"`
+	UnreadCount sql.NullInt64 		`json:"unread_count"`
+	LastMsg     sql.NullString     `json:"last_msg"`
 }
